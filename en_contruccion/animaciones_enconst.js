41574ac66d43fd300ff6b2a1e3d4a5e061a1a066
thomys57364 <124540978+thomys57364@users.noreply.github.com>
2025-03-30 11:28:22 -0500

    Add files via upload

---
+++ en_contruccion/animaciones_enconst.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    const elementosSlide = document.querySelectorAll('.slide-in-left');
+    elementosSlide.forEach((el) => el.classList.add('visible-slide'));
+  });
+  
