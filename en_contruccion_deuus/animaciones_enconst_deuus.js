3daae9ebfe8a09afed74cf7038500734ff7e886b
thomys57364 <124540978+thomys57364@users.noreply.github.com>
2025-03-30 11:26:20 -0500

    Add files via upload

---
+++ en_contruccion_deuus/animaciones_enconst_deuus.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+  const elementosSlide = document.querySelectorAll('.slide-in-left');
+  elementosSlide.forEach((el) => el.classList.add('visible-slide'));
+});
