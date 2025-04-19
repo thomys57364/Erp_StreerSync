492abdca5f0bcd7751b6794e92b39164895f254d
Jullians <julliansamado@gmail.com>
2025-04-18 19:37:07 -0500

    meti los generos en una carpeta, cree la parte de mujer, y acomode unas cosas en codigo sobre la nueva carpeta de genero y sobre pantallas pequeñas

---
+++ generos/parte_mujer/mujer.js
@@ -0,0 +1,9 @@
+// Animación en cascada al cargar
+window.addEventListener('load', () => {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+
+    fadeInElements.forEach((element, index) => {
+      element.style.animationDelay = `${index * 0.10}s`; // Cascada rápida
+      element.style.animationPlayState = 'running';
+    });
+  });
